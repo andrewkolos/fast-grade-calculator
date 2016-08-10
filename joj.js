bdc583d8cd59e9b64f7acc82d014bb139d740794
Andrew Kolos <andrewrkolos@gmail.com>
2016-08-10 02:08:02 -0400

    totals and formatting

--- joj.js
+++ joj.js
@@ -21,8 +21,9 @@ window.onload = function() {
 // On focus, an editable span will be reaplaced with a text input initialized
 // with the value of the span. On blur, the text input will be replaced with 
 // the hidden span, updated with the new value.
+// Note that these spans cannot be edited to be an empty string.
 function initEditables() {
-	var editables = document.getElementsByClassName("editable");
+	var editables = document.getElementsByClassName("editable"); // IE9+
 	for (var i = 0; i < editables.length; i++) {
 		editables[i].onfocus = function(event) {
 			var span, input, text;
@@ -30,20 +31,21 @@ function initEditables() {
 			if (span && span.tagName.toUpperCase() === "SPAN") {
 				span.style.display = "none"; // hide
 				
-				text = span.innerHTML; // retrieve text
+				text = span.textContent; // retrieve text
 				
 				input = document.createElement("input");
 				input.type = "text";
-				input.value = span.innerHTML;
-				input.size = Math.max(text.length / 4 * 3, 4);
-				input.select();
-				span.parentNode.insertBefore(input, span);
+				input.value = span.textContent;
+				input.size = text.length / 2;
 				
-				input.focus();
+				span.parentNode.insertBefore(input, span);
+				//input.focus();
+				input.select();
 				
 				input.onblur = function() {
 					span.parentNode.removeChild(input);
-					span.innerHTML = input.value;
+					if (input.value !== "") // edit to empty string will be ignored
+						span.textContent = input.value;
 					span.style.display = "";
 					updateGrade();
 				}
@@ -58,10 +60,10 @@ function toggleNameColumn(cb) {
 	var footer = table.rows[rowCount-1];
 	var cspan = footer.cells[0].colSpan;
 	
-	for (var i = 0; i < rowCount-1; i++) {
+	for (var i = 0; i < rowCount; i++) {
 		table.rows[i].cells[0].style.display = cb.checked ? "" : "none";
 	}
-	footer.cells[0].colSpan = cb.checked ? cspan + 1 : cspan - 1;
+	//footer.cells[0].colSpan = cb.checked ? cspan + 1 : cspan - 1;
 	focusFirstCell();
 }
 
@@ -69,27 +71,29 @@ function updateGrade() {
 	var totalEarned = 0.0;
 	var totalWeight = 0.0;
 	var gradeLabel = document.getElementById("gradeLabel");
-	var letter = "N/A"
-	
 	var table = document.getElementById("mainTable");
+	var totalEarnedLabel = document.getElementById("totalEarned");
+	var totalPossibleLabel = document.getElementById("totalPossible");
 	
 	// TODO eww literals. find a more elegant solution.
 	for (var i = 1; i < table.rows.length-1; i++) { 
 		var cells = table.rows[i].cells;
-		var score = parseInt(cells[1].getElementsByTagName("span")[0].innerHTML) / 100;
-		var weight = parseInt(cells[2].getElementsByTagName("span")[0].innerHTML);
-		var earned = (weight*score);
+		var score = parseFloat(cells[1].getElementsByTagName("span")[0].textContent) / 100.0;
+		var weight = parseFloat(cells[2].getElementsByTagName("span")[0].textContent);
+		var earned = (weight*1.0*score);
 		totalEarned = totalEarned + earned;
 		totalWeight = totalWeight + weight;
-		cells[3].innerHTML = earned.toFixed(2);
+		cells[3].textContent = earned.toFixed(2);
 	}
 	
-	var grade = (totalEarned/totalWeight*100);
+	var grade = (totalEarned*1.0/totalWeight*100);
 	
 	displayedGrade = grade.toFixed(2);
+	totalEarnedLabel.textContent = totalEarned.toFixed(2);
+	totalPossibleLabel.textContent = totalWeight.toFixed(2);
 	
 	if (!Number.isNaN(grade)) {
-		gradeLabel.innerHTML = "Current grade: " + displayedGrade;
+		gradeLabel.textContent = "Current grade: " + displayedGrade;
 		document.getElementById("warningLabel").style.visibility = totalWeight == 100 ? "hidden" : "";
 	}
 }
